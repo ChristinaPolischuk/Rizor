4ef118c4f63781f33f079f0994f309c5ab985164
frontEndDevel <front_end_dev@yahoo.com>
2020-11-18 09:09:00 +0200

    scroll for sections done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,8 @@
 @@include("libs/slick.min.js");
+@@include("libs/fullpage.min.js");
 
 (function () {
 	@@include("scripts/components/_header.js");
 	@@include("scripts/components/_product.js");
+	@@include("scripts/components/_fullpage-settings.js");
 })();
---
+++ #src/js/scripts/components/_fullpage-settings.js
@@ -0,0 +1,9 @@
+let fullPage = function () {
+	$('#fullpage').fullpage({
+		autoScrolling: true,
+		navigation: true,
+		scrollBar: true,
+	});
+}
+
+fullPage();
